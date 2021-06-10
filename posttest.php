c9618a6b24a6a2abd1fafbb4b7ac6cdd1bd5f2bb
KarlRenauld <83756553+KarlRenauld@users.noreply.github.com>
2021-06-10 20:38:58 +0200

    Delete posttest.php

--- posttest.php
+++
@@ -1,8 +0,0 @@
-<?php
-include_once('config.php');
-        $req = $connection-> prepare("INSERT INTO posts (bank_id, comment, user_id) VALUES (?, ?, ?)");
-        $data = $req->execute([$_COOKIE['acteur_id'], $_POST['comment'], $_POST['author']]);
-        //header('Location: acteur.php?acteur='. $_COOKIE['acteur_id'] .'');
-        var_dump([$_COOKIE['acteur_id']]);
-        var_dump([$_POST['comment']]);
-        var_dump([$_POST['author']]);
